e94aa6384a6b8a951bf1fdddf8d78eab0dfc3602
Matthew <mmdstanley@gmail.com>
2017-03-25 16:56:30 -0700

    Started working on template file

---
+++ template.py
@@ -0,0 +1,17 @@
+import time
+import RPI.GPIO as GPIO
+from subprocess import call
+
+#Set up constants
+RIGHT = 25
+LEFT = 24
+FORWARD = 18
+BACKWARD = 23
+
+def turnRight():
+
+def turnLeft():
+
+def moveForward():
+
+def moveBackward():
