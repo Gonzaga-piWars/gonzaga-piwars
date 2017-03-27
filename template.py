68935ccd4216c3605bcb432540bd472d93bd8a51
Matthew <mmdstanley@gmail.com>
2017-03-26 17:17:19 -0700

    Test for template

--- template.py
+++ template.py
@@ -8,6 +8,23 @@ LEFT = 24
 FORWARD = 18
 BACKWARD = 23
 
+run = True
+
+GPIO.setup(RIGHT,GPIO.OUT)
+GPIO.setup(LEFT,GPIO.OUT)
+GPIO.setup(FORWARD,GPIO.OUT)
+
+GPIO.output(FORWARD,True)
+time.sleep(5)
+GPIO.output(RIGHT,True)
+time.sleep(.005)
+GPIO.output(RIGHT,False)
+time.sleep(5)
+GPIO.output(LEFT,True)
+time.sleep(.005)
+time.sleep(2)
+GPIO.output(FORWARD,False)
+
 def turnRight():
 
 def turnLeft():
