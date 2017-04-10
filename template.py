711ecad2f3860a4bba63d8e5d4ddb9cf259aaed7
Matthew <mmdstanley@gmail.com>
2017-04-09 17:06:11 -0700

    Converted Module into working Class

--- template.py
+++ template.py
@@ -2,42 +2,42 @@ import time
 import RPi.GPIO as GPIO
 from subprocess import call
 
-#Set up constants
-RIGHT = 25
-LEFT = 24
-FORWARD = 18
-BACKWARD = 23
+class Controller(object):
 
-WAIT = 0.005
+    #Set up constants
+    RIGHT = 25
+    LEFT = 24
+    FORWARD = 18
+    BACKWARD = 23
 
-run = True
+        def __init__(self):
+            GPIO.setmode(GPIO.BCM)
 
-GPIO.setmode(GPIO.BCM)
+            GPIO.setup(RIGHT,GPIO.OUT)
+            GPIO.setup(LEFT,GPIO.OUT)
+            GPIO.setup(FORWARD,GPIO.OUT)
+            GPIO.setup(BACKWARD,GPIO.OUT)
 
-GPIO.setup(RIGHT,GPIO.OUT)
-GPIO.setup(LEFT,GPIO.OUT)
-GPIO.setup(FORWARD,GPIO.OUT)
+        def turnRight():
+            GPIO.output(RIGHT,True)
+            GPIO.output(LEFT,False)
 
-GPIO.output(FORWARD,True)
-time.sleep(5)
-GPIO.output(RIGHT,True)
-time.sleep(2)
-GPIO.output(RIGHT,False)
-time.sleep(5)
-GPIO.output(LEFT,True)
-time.sleep(2)
-GPIO.output(LEFT,False)
-time.sleep(2)
-GPIO.output(FORWARD,False)
+        def turnLeft():
+            GPIO.output(LEFT,True)
+            GPIO.output(RIGHT,False)
 
-#def turnRight():
+        def moveForward():
+            GPIO.output(FORWARD,True)
+            GPIO.output(BACKWARD,False)
 
-#def turnLeft():
+        def moveBackward():
+            GPIO.output(BACKWARD,True)
+            GPIO.output(FORWARD,False)
 
-#def moveForward():
+        def stop():
+            GPIO.output(FORWARD,False)
+            GPIO.output(BACKWARD,False)
 
-#def moveBackward():
-
-#def stop():
-
-#def goStraight():
+        def goStraight():
+            GPIO.output(RIGHT,False)
+            GPIO.output(LEFT,False)
