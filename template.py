acc1d37be8510630040af35bbaf6a45e5b25b81a
Matthew <mmdstanley@gmail.com>
2017-04-21 23:03:56 -0700

    Making last stand

--- template.py
+++ template.py
@@ -12,50 +12,50 @@ class Controller(object):
     BACKWARD = 23
     EMERGENT = 14
 
-        def __init__(self):
-            GPIO.setmode(GPIO.BCM)
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
 
-            GPIO.setup(RIGHT,GPIO.OUT)
-            GPIO.setup(LEFT,GPIO.OUT)
-            GPIO.setup(FORWARD,GPIO.OUT)
-            GPIO.setup(BACKWARD,GPIO.OUT)
-            GPIO.setup(EMERGENT,GPIO.OUT)
+        GPIO.setup(RIGHT,GPIO.OUT)
+        GPIO.setup(LEFT,GPIO.OUT)
+        GPIO.setup(FORWARD,GPIO.OUT)
+        GPIO.setup(BACKWARD,GPIO.OUT)
+        GPIO.setup(EMERGENT,GPIO.OUT)
 
-            stop()
-            goStraight()
-            GPIO.output(EMERGENT,False)
+        stop()
+        goStraight()
+        GPIO.output(EMERGENT,False)
 
-        def turnRight():
-            GPIO.output(RIGHT,True)
-            GPIO.output(LEFT,False)
+    def turnRight():
+        GPIO.output(RIGHT,True)
+        GPIO.output(LEFT,False)
 
-        def turnLeft():
-            GPIO.output(LEFT,True)
-            GPIO.output(RIGHT,False)
+    def turnLeft():
+        GPIO.output(LEFT,True)
+        GPIO.output(RIGHT,False)
 
-        def moveForward():
-            GPIO.output(FORWARD,True)
-            GPIO.output(BACKWARD,False)
+    def moveForward():
+        GPIO.output(FORWARD,True)
+        GPIO.output(BACKWARD,False)
 
-        def moveBackward():
-            GPIO.output(BACKWARD,True)
-            GPIO.output(FORWARD,False)
+    def moveBackward():
+        GPIO.output(BACKWARD,True)
+        GPIO.output(FORWARD,False)
 
-        def stop():
-            GPIO.output(FORWARD,False)
-            GPIO.output(BACKWARD,False)
+    def stop():
+        GPIO.output(FORWARD,False)
+        GPIO.output(BACKWARD,False)
 
-        def goStraight():
-            GPIO.output(RIGHT,False)
-            GPIO.output(LEFT,False)
+    def goStraight():
+        GPIO.output(RIGHT,False)
+        GPIO.output(LEFT,False)
 
-        def emergent():
+    def emergent():
+        keys = pygame.key.get_pressed()
+        stop()
+        goStraight()
+        GPIO.output(EMERGENT,True)
+        while keys[pygame.K_RETURN]:
+            time.sleep(1)
             keys = pygame.key.get_pressed()
-            stop()
-            goStraight()
-            GPIO.output(EMERGENT,True)
-            while keys[pygame.K_RETURN]:
-                time.sleep(1)
-                keys = pygame.key.get_pressed()
-            GPIO.output(EMERGENT,False)
-                
+        GPIO.output(EMERGENT,False)
+            
